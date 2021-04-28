4fb76171fab6c68563a1783f860e4a22d8793f61
BrianLevin <bml201095@gmail.com>
2021-04-28 13:08:01 -0400

    created a global array which will store  the pushed posts

--- app.js
+++ app.js
@@ -15,10 +15,13 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+const posts = [];
 
 app.get("/", function(req,res){
 
   res.render("home", {startingContent: homeStartingContent});
+
+  console.log("posts");
 
 });
 
@@ -49,6 +52,10 @@ title:  req.body.postTitle,
 content: req.body.postBody
 
   };
+
+  posts.push(post);
+
+  res.redirect(/)
   
   } );
 
